e2a417dd993a8ac7fabb974b2773abcee442b554
Yuh Mahp <pdh372@gmail.com>
2023-11-18 16:07:30 +0700

    refactor: base source

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+
+const Profile =  () => {
+  
+
+  return (
+    <div>
+      <div><div>
+      <h2>Shop Name</h2></div>
+      <div>
+      <input type="text" /></div></div>
+    </div>
+  );
+};
+
+
+export default Profile;
